1b0dd1fdbb799596090745daa356f91db9736bf5
Soheilstar72 <132057572+Soheilstar72@users.noreply.github.com>
2025-03-09 09:51:49 +0100

    Delete Solutions/650_Questions/Chapter 1/Problem 7.py

--- Solutions/650_Questions/Chapter 1/Problem 7.py
+++
@@ -1,6 +0,0 @@
-real=float(input('Please Enter the Real part of your Complex number:'))
-imag=float(input('Please Enter the Imag part of your Complex number:'))
-
-Number=complex(real,imag)
-
-print('Your Number is :' , Number)
