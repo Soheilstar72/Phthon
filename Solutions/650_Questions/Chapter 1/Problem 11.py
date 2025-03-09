e12280aa9a14b7be5853b6e1505c5759d28d2606
Soheilstar72 <132057572+Soheilstar72@users.noreply.github.com>
2025-03-09 09:50:48 +0100

    Delete Solutions/650_Questions/Chapter 1/Problem 11.py

--- Solutions/650_Questions/Chapter 1/Problem 11.py
+++
@@ -1,9 +0,0 @@
-first_price=float(input('Please Enter the Pervious year price:'))
-second_price=float(input('Please Enter the Current price:'))
-
-increase=round((second_price/first_price - 1)*100)
-
-new_price=second_price*second_price/first_price
-
-print(f'The increase is equal to {increase} %')
-print('Next year price will be equal to : ',new_price)
