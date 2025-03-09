ba62f6a78d002a1f4166f79beeba2f39c5aa10a5
Soheilstar72 <132057572+Soheilstar72@users.noreply.github.com>
2025-03-09 09:51:19 +0100

    Delete Solutions/650_Questions/Chapter 1/Problem 3.py

--- Solutions/650_Questions/Chapter 1/Problem 3.py
+++
@@ -1,7 +0,0 @@
-radius=float(input('Please Enter the Radius of Spherical:'))
-
-volume= radius * radius * radius * 3.1415 * 4/3
-area= 4 * radius * radius * 3.1415 
-
-print('the Volume is equal to %f' %volume)
-print('the Area is equal to %f' %area)
