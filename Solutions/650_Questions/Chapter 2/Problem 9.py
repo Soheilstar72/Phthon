f1521a3207f67430ffe5d45cbd781b0259b85ddf
Soheilstar72 <132057572+Soheilstar72@users.noreply.github.com>
2025-03-09 09:52:36 +0100

    Delete Solutions/650_Questions/Chapter 2 directory

--- Solutions/650_Questions/Chapter 2/Problem 9.py
+++
@@ -1,7 +0,0 @@
-years=int(input("How many years do you need:"))
-price=int(input('The Value of Service:'))
-inflation = float(input('What is your forecast for inflation rate (%):'))
-
-for i in range(years):
-    price = price + price*inflation/100
-    print(f'Price is equal to {price} in year {i+1}')
